c926012eab9c2e80c3185dd26037e6164f5673c3
Cristian Fernandez <cristian.fernandez@altovita.com>
2023-10-09 23:32:16 +0200

    feat: header component

---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,3 @@
+<header>
+    <h1 class="h1">NoDijkstra Website</h1>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,20 @@
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+    import {AppShell, storePopup} from '@skeletonlabs/skeleton';
+    import Header from "$components/Header/Header.svelte";
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<slot />
+
+<AppShell>
+    <svelte:fragment slot="header"><Header /></svelte:fragment>
+    <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+    <!-- (sidebarRight) -->
+    <svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+    <!-- Router Slot -->
+    <slot />
+    <!-- ---- / ---- -->
+    <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+    <svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
