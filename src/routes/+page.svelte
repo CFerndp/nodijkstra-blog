06e5de20516d501f588d75b4e44ee459f6cab085
Cristian Fernandez <cristian.fernandez@altovita.com>
2023-10-10 02:40:32 +0200

    feat: add home content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,47 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-		Hello there :D
-	</div>
+    <div class="space-y-5">
+        <h1 class="h1">¡Bienvenido a mi pequeño rincon!</h1>
+
+        <p>
+            ¡Hola y bienvenido! Soy Cristian Fernandez, un desarrollador de software apasionado por crear soluciones usando la
+            tecnología.</p>
+        <p> En este espacio, compartiré mi viaje profesional, proyectos destacados y pensamientos sobre arquitectura de
+            software, desarrollo de frontend y curiosidades varias.</p>
+        <p> Mi objetivo es conectarme con personas
+            afines,
+            colaborar en proyectos emocionantes y continuar aprendiendo y creciendo en mi campo.
+            Además viajo bastante y me encanta demostrar que a día de hoy se puede trabajar desde cualquier punto del
+            planeta :P
+        </p>
+        <p>
+            Mi misión es aprender y actualizarme en las tic. A través del desarrollo de software, busco resolver
+            problemas y ver el mundo cambiar. Cada proyecto es una oportunidad para hacer que las ideas cobren vida y
+            marquen la diferencia.
+        </p>
+        <h2 class="h2">¿Qué puedes hacer por aquí?</h2>
+        <ol class="list">
+            <li>
+                <span class="badge bg-primary-500">1.</span>
+                <span class="flex-auto">Explora mi CV para conocer más sobre mi experiencia y habilidades.</span>
+            </li>
+            <li>
+                <span class="badge bg-primary-500">2.</span>
+                <span class="flex-auto">Sumérgete en mi Blog y descubre artículos sobre tecnología y desarrollo de software.</span>
+            </li>
+            <li>
+                <span class="badge bg-primary-500">3.</span>
+                <span class="flex-auto">¿Tienes un proyecto en mente? ¡No dudes en contactarme y hablemos sobre cómo puedo ayudarte.</span>
+            </li>
+            <li>
+                <span class="badge bg-primary-500">4.</span>
+                <span class="flex-auto">Descubre más sobre mí en la sección Acerca de Mí, donde comparto mi historia y pasiones personales.</span>
+            </li>
+        </ol>
+        <p>
+            ¡Gracias por visitar mi sitio web! Espero que encuentres contenido interesante y valioso. PD: ¡Gatitos!
+        </p>
+    </div>
 </div>
+
